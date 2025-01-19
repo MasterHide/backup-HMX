b92aa72dd4f97b08f9b54ed54abfae7cedc24cbf
x404 MASTER <71309968+MasterHide@users.noreply.github.com>
2025-01-19 09:02:50 +0530

    Update backup-HMX.sh

--- backup-HMX.sh
+++ backup-HMX.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Bot token
-# گرفتن توکن ربات از کاربر و ذخیره آن در متغیر tk
+# Get the bot token from the user and store it in the variable 'tk'
 while [[ -z "$tk" ]]; do
     echo "Bot token: "
     read -r tk
@@ -11,29 +11,29 @@ while [[ -z "$tk" ]]; do
     fi
 done
 
-# Chat id
-# گرفتن Chat ID از کاربر و ذخیره آن در متغیر chatid
+# Chat ID
+# Get the Chat ID from the user and store it in the variable 'chatid'
 while [[ -z "$chatid" ]]; do
-    echo "Chat id: "
+    echo "Chat ID: "
     read -r chatid
     if [[ $chatid == $'\0' ]]; then
-        echo "Invalid input. Chat id cannot be empty."
+        echo "Invalid input. Chat ID cannot be empty."
         unset chatid
     elif [[ ! $chatid =~ ^\-?[0-9]+$ ]]; then
-        echo "${chatid} is not a number."
+        echo "${chatid} is not a valid number."
         unset chatid
     fi
 done
 
 # Caption
-# گرفتن عنوان برای فایل پشتیبان و ذخیره آن در متغیر caption
-echo "Caption (for example, your domain, to identify the database file more easily): "
+# Get a caption for the backup file, for easier identification (e.g., your domain)
+echo "Caption (e.g., your domain to identify the database file more easily): "
 read -r caption
 
 # Cronjob
-# تعیین زمانی برای اجرای این اسکریپت به صورت دوره‌ای
+# Set the schedule for running the script periodically
 while true; do
-    echo "Cronjob (minutes and hours) (e.g : 30 6 or 0 12) : "
+    echo "Cronjob (minutes and hours) (e.g: 30 6 or 0 12): "
     read -r minute hour
     if [[ $minute == 0 ]] && [[ $hour == 0 ]]; then
         cron_time="* * * * *"
@@ -52,23 +52,23 @@ while true; do
     fi
 done
 
-
-# x-ui or marzban or hiddify
-# گرفتن نوع نرم افزاری که می‌خواهیم پشتیبانی از آن بگیریم و ذخیره آن در متغیر xmh
+# x-ui, Marzban or Hiddify?
+# Ask the user to select which software backup to perform
 while [[ -z "$xmh" ]]; do
-    echo "x-ui or marzban or hiddify? [x/m/h] : "
+    echo "Choose software (x-ui, Marzban or Hiddify) [x/m/h]: "
     read -r xmh
     if [[ $xmh == $'\0' ]]; then
-        echo "Invalid input. Please choose x, m or h."
+        echo "Invalid input. Please choose x, m, or h."
         unset xmh
     elif [[ ! $xmh =~ ^[xmh]$ ]]; then
-        echo "${xmh} is not a valid option. Please choose x, m or h."
+        echo "${xmh} is not a valid option. Please choose x, m, or h."
         unset xmh
     fi
 done
 
+# Ask if the previous crontabs should be cleared
 while [[ -z "$crontabs" ]]; do
-    echo "Would you like the previous crontabs to be cleared? [y/n] : "
+    echo "Would you like to clear the previous crontabs? [y/n]: "
     read -r crontabs
     if [[ $crontabs == $'\0' ]]; then
         echo "Invalid input. Please choose y or n."
@@ -79,151 +79,139 @@ while [[ -z "$crontabs" ]]; do
     fi
 done
 
+# If yes, remove previous cronjobs
 if [[ "$crontabs" == "y" ]]; then
-# remove cronjobs
-sudo crontab -l | grep -vE '/root/ac-backup.+\.sh' | crontab -
+    sudo crontab -l | grep -vE '/root/backup-HMX.+\.sh' | crontab -
 fi
 
-
-# m backup
-# ساخت فایل پشتیبانی برای نرم‌افزار Marzban و ذخیره آن در فایل ac-backup.zip
+# Marzban Backup (removed upload section)
+# Create a backup file for Marzban software and store it in backup-HMX.zip
 if [[ "$xmh" == "m" ]]; then
 
-if dir=$(find /opt /root -type d -iname "marzban" -print -quit); then
-  echo "The folder exists at $dir"
-else
-  echo "The folder does not exist."
-  exit 1
-fi
+    if dir=$(find /opt /root -type d -iname "marzban" -print -quit); then
+        echo "The folder exists at $dir"
+    else
+        echo "The folder does not exist."
+        exit 1
+    fi
 
-if [ -d "/var/lib/marzban/mysql" ]; then
+    if [ -d "/var/lib/marzban/mysql" ]; then
+        sed -i -e 's/\s*=\s*/=/' -e 's/\s*:\s*/:/' -e 's/^\s*//' /opt/marzban/.env
+        docker exec marzban-mysql-1 bash -c "mkdir -p /var/lib/mysql/db-backup"
+        source /opt/marzban/.env
 
-  sed -i -e 's/\s*=\s*/=/' -e 's/\s*:\s*/:/' -e 's/^\s*//' /opt/marzban/.env
-
-  docker exec marzban-mysql-1 bash -c "mkdir -p /var/lib/mysql/db-backup"
-  source /opt/marzban/.env
-
-    cat > "/var/lib/marzban/mysql/ac-backup.sh" <<EOL
+        cat > "/var/lib/marzban/mysql/backup-HMX.sh" <<EOL
 #!/bin/bash
 
 USER="root"
-PASSWORD="$MYSQL_ROOT_PASSWORD"
-
+PASSWORD="\$MYSQL_ROOT_PASSWORD"
 
 databases=\$(mysql --user=\$USER --password=\$PASSWORD -e "SHOW DATABASES;" | tr -d "| " | grep -v Database)
 
 for db in \$databases; do
     if [[ "\$db" != "information_schema" ]] && [[ "\$db" != "mysql" ]] && [[ "\$db" != "performance_schema" ]] && [[ "\$db" != "sys" ]] ; then
         echo "Dumping database: \$db"
-		mysqldump --force --opt --user=\$USER --password=\$PASSWORD --databases \$db > /var/lib/mysql/db-backup/\$db.sql
-
+        mysqldump --force --opt --user=\$USER --password=\$PASSWORD --databases \$db > /var/lib/mysql/db-backup/\$db.sql
     fi
 done
-
 EOL
-chmod +x /var/lib/marzban/mysql/ac-backup.sh
+        chmod +x /var/lib/marzban/mysql/backup-HMX.sh
 
-ZIP=$(cat <<EOF
-docker exec marzban-mysql-1 bash -c "/var/lib/mysql/ac-backup.sh"
-zip -r /root/ac-backup-m.zip /opt/marzban/* /var/lib/marzban/* /opt/marzban/.env -x /var/lib/marzban/mysql/\*
-zip -r /root/ac-backup-m.zip /var/lib/marzban/mysql/db-backup/*
+        ZIP=$(cat <<EOF
+docker exec marzban-mysql-1 bash -c "/var/lib/mysql/backup-HMX.sh"
+zip -r /root/backup-HMX.zip /opt/marzban/* /var/lib/marzban/* /opt/marzban/.env -x /var/lib/marzban/mysql/*
+zip -r /root/backup-HMX.zip /var/lib/marzban/mysql/db-backup/*
 rm -rf /var/lib/marzban/mysql/db-backup/*
 EOF
-)
-
     else
-      ZIP="zip -r /root/ac-backup-m.zip ${dir}/* /var/lib/marzban/* /opt/marzban/.env"
+        ZIP="zip -r /root/backup-HMX.zip ${dir}/* /var/lib/marzban/* /opt/marzban/.env"
+    fi
+
+    MasterHide="MasterHide Marzban backup"
+
 fi
 
-ACLover="marzban backup"
-
-# x-ui backup
-# ساخت فایل پشتیبانی برای نرم‌افزار X-UI و ذخیره آن در فایل ac-backup.zip
+# x-ui Backup
+# Create a backup file for X-UI software and store it in backup-HMX.zip
 elif [[ "$xmh" == "x" ]]; then
 
-if dbDir=$(find /etc -type d -iname "x-ui*" -print -quit); then
-  echo "The folder exists at $dbDir"
-else
-  echo "The folder does not exist."
-  exit 1
-fi
+    if dbDir=$(find /etc -type d -iname "x-ui*" -print -quit); then
+        echo "The folder exists at $dbDir"
+    else
+        echo "The folder does not exist."
+        exit 1
+    fi
 
-if configDir=$(find /usr/local -type d -iname "x-ui*" -print -quit); then
-  echo "The folder exists at $configDir"
-else
-  echo "The folder does not exist."
-  exit 1
-fi
+    if configDir=$(find /usr/local -type d -iname "x-ui*" -print -quit); then
+        echo "The folder exists at $configDir"
+    else
+        echo "The folder does not exist."
+        exit 1
+    fi
 
-ZIP="zip /root/ac-backup-x.zip ${dbDir}/x-ui.db ${configDir}/config.json"
-ACLover="x-ui backup"
+    ZIP="zip /root/backup-HMX-x.zip ${dbDir}/x-ui.db ${configDir}/config.json"
+    MasterHide="MasterHide x-ui backup"
 
-# hiddify backup
-# ساخت فایل پشتیبانی برای نرم‌افزار Hiddify و ذخیره آن در فایل ac-backup.zip
+# Hiddify Backup
+# Create a backup file for Hiddify software and store it in backup-HMX.zip
 elif [[ "$xmh" == "h" ]]; then
 
-if ! find /opt/hiddify-config/hiddify-panel/ -type d -iname "backup" -print -quit; then
-  echo "The folder does not exist."
-  exit 1
-fi
+    if ! find /opt/hiddify-config/hiddify-panel/ -type d -iname "backup" -print -quit; then
+        echo "The folder does not exist."
+        exit 1
+    fi
 
-ZIP=$(cat <<EOF
+    ZIP=$(cat <<EOF
 cd /opt/hiddify-config/hiddify-panel/
-if [ $(find /opt/hiddify-config/hiddify-panel/backup -type f | wc -l) -gt 100 ]; then
+if [ \$(find /opt/hiddify-config/hiddify-panel/backup -type f | wc -l) -gt 100 ]; then
   find /opt/hiddify-config/hiddify-panel/backup -type f -delete
 fi
 python3 -m hiddifypanel backup
 cd /opt/hiddify-config/hiddify-panel/backup
 latest_file=\$(ls -t *.json | head -n1)
-rm -f /root/ac-backup-h.zip
-zip /root/ac-backup-h.zip /opt/hiddify-config/hiddify-panel/backup/\$latest_file
-
+rm -f /root/backup-HMX-h.zip
+zip /root/backup-HMX-h.zip /opt/hiddify-config/hiddify-panel/backup/\$latest_file
 EOF
 )
-ACLover="hiddify backup"
+    MasterHide="MasterHide Hiddify backup"
 else
-echo "Please choose m or x or h only !"
-exit 1
+    echo "Please choose m, x, or h only!"
+    exit 1
 fi
 
-
+# Trim the caption
 trim() {
-    # remove leading and trailing whitespace/lines
     local var="$*"
-    # remove leading whitespace characters
     var="${var#"${var%%[![:space:]]*}"}"
-    # remove trailing whitespace characters
     var="${var%"${var##*[![:space:]]}"}"
     echo -n "$var"
 }
 
 IP=$(ip route get 1 | sed -n 's/^.*src \([0-9.]*\) .*$/\1/p')
-caption="${caption}\n\n${ACLover}\n<code>${IP}</code>\nCreated by @AC_Lover - https://github.com/AC-Lover/backup"
+caption="${caption}\n\n${MasterHide}\n<code>${IP}</code>\nCreated by @MasterHide - https://github.com/MasterHide/backup"
 comment=$(echo -e "$caption" | sed 's/<code>//g;s/<\/code>//g')
 comment=$(trim "$comment")
 
-# install zip
-# نصب پکیج zip
+# Install zip
+# Install the 'zip' package if it's not already installed
 sudo apt install zip -y
 
-# send backup to telegram
-# ارسال فایل پشتیبانی به تلگرام
-cat > "/root/ac-backup-${xmh}.sh" <<EOL
-rm -rf /root/ac-backup-${xmh}.zip
+# Send the backup to Telegram
+# Send the backup file to the Telegram bot
+cat > "/root/backup-HMX-${xmh}.sh" <<EOL
+rm -rf /root/backup-HMX-${xmh}.zip
 $ZIP
-echo -e "$comment" | zip -z /root/ac-backup-${xmh}.zip
-curl -F chat_id="${chatid}" -F caption=\$'${caption}' -F parse_mode="HTML" -F document=@"/root/ac-backup-${xmh}.zip" https://api.telegram.org/bot${tk}/sendDocument
+echo -e "$comment" | zip -z /root/backup-HMX-${xmh}.zip
+curl -F chat_id="${chatid}" -F caption=\$'${caption}' -F parse_mode="HTML" -F document=@"/root/backup-HMX-${xmh}.zip" https://api.telegram.org/bot${tk}/sendDocument
 EOL
 
+# Add cronjob for periodic execution
+{ crontab -l -u root; echo "${cron_time} /bin/bash /root/backup-HMX-${xmh}.sh >/dev/null 2>&1"; } | crontab -u root -
 
-# Add cronjob
-# افزودن کرانجاب جدید برای اجرای دوره‌ای این اسکریپت
-{ crontab -l -u root; echo "${cron_time} /bin/bash /root/ac-backup-${xmh}.sh >/dev/null 2>&1"; } | crontab -u root -
+# Run the script
+bash "/root/backup-HMX-${xmh}.sh"
 
-# run the script
-# اجرای این اسکریپت
-bash "/root/ac-backup-${xmh}.sh"
+# Clean up
+rm -f /root/backup-HMX-${xmh}.sh /root/backup-HMX-${xmh}.zip
 
-# Done
-# پایان اجرای اسکریپت
 echo -e "\nDone\n"
